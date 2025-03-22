c796e5ab7071a92fa319e03209406f16b10e9197
Likhith Sai Charan Jupudi <jupudilikhithsaicharan@gmail.com>
2025-03-22 13:00:10 +0530

    Completed all required functions in script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(emp=>{
+    if(emp.profession === "developer"){
+      console.log(emp);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(emp=>{
+    if(emp.profession === "developer"){
+      console.log(emp);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmp={id: 4, name: "susan", age: "20", profession: "intern"}
+  arr.push(newEmp);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr=arr.filter(emp=>emp.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 5, name: "alice", age: "22", profession: "manager" },
+    { id: 6, name: "bob", age: "24", profession: "designer" },
+    { id: 7, name: "eve", age: "21", profession: "tester" }
+  ];
+  const cArr=arr.concat(newArr);
+  console.log(cArr);
 }
